442b18949b55a82158dcf8b389ddb43608f31fea
clodlin <cmantsila0@gmail.com>
2025-04-10 00:06:57 +0100

    modificaiton de gitignore

--- git_observer/utils.py
+++ git_observer/utils.py
@@ -9,3 +9,4 @@ init(autoreset=True)
 def get_current_directory():
     """Récupération du dossier courant (compatible Linux & Windows)"""
     return os.getcwd() if platform.system() == "Windows" else subprocess.run(["pwd"], capture_output=True, text=True).stdout.strip()
+
