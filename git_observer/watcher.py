21a10883b9b7f4d261d31cd0e66d7c3a3f494c8d
clodlin <cmantsila0@gmail.com>
2025-04-13 12:52:01 +0100

    \message\ et retourne le message.\\\\n        try:\n            with open(file_path, \r\, encoding=\utf-8\) as f:\n                for line in f:\n                    if \commit_name=\ in line:\n                        parts = line.split(\commit_name=\, 1),

--- git_observer/watcher.py
+++ git_observer/watcher.py
@@ -155,7 +155,7 @@ class GitAutoCommitHandler(FileSystemEventHandler):
             MODIFIED_FILES.clear()
             return
     
-        if(self.m)
+        if(self.max_files)
         commit_message = self.default_message if self.default_message else self.generate_commit_message()
     
     
